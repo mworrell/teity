b70f3c1c16c1b670925e3df7848d0924bc729ec4
Marc Worrell <marc@worrell.nl>
2013-11-30 21:53:14 +0100

    Initial commit.

---
+++ teity.erl
@@ -0,0 +1,31 @@
+%% @author Marc Worrell
+%% @copyright 2013 Marc Worrell
+%% Generated on 2013-11-30
+%% @doc This site was based on the 'blog' skeleton.
+
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(teity).
+-author("Marc Worrell").
+
+-mod_title("Teity zotonic site").
+-mod_description("Teity, web site for Lily Ching.").
+-mod_prio(10).
+
+-include_lib("zotonic.hrl").
+
+
+%%====================================================================
+%% support functions go here
+%%====================================================================
+
